9d7d747f10bab16b3d85915a5b87e10acd2374f0
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-11-04 15:21:02 -0400

    lttng context: perf counter, fix 32-bit vs 64-bit field size bug
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- lttng-context-perf-counters.c
+++ lttng-context-perf-counters.c
@@ -229,9 +229,9 @@ int lttng_add_perf_counter_to_ctx(uint32_t type,
 
 	field->event_field.name = name_alloc;
 	field->event_field.type.atype = atype_integer;
-	field->event_field.type.u.basic.integer.size = sizeof(unsigned long) * CHAR_BIT;
-	field->event_field.type.u.basic.integer.alignment = ltt_alignof(unsigned long) * CHAR_BIT;
-	field->event_field.type.u.basic.integer.signedness = is_signed_type(unsigned long);
+	field->event_field.type.u.basic.integer.size = sizeof(uint64_t) * CHAR_BIT;
+	field->event_field.type.u.basic.integer.alignment = ltt_alignof(uint64_t) * CHAR_BIT;
+	field->event_field.type.u.basic.integer.signedness = is_signed_type(uint64_t);
 	field->event_field.type.u.basic.integer.reverse_byte_order = 0;
 	field->event_field.type.u.basic.integer.base = 10;
 	field->event_field.type.u.basic.integer.encoding = lttng_encode_none;
