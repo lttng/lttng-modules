49bafb1440249c0f4e7d5161ef0ef8ae871102bf
Simon Marchi <simon.marchi@polymtl.ca>
2012-11-23 18:10:38 -0500

    Fix compilation for 3.0 branch (>= 3.0.39)
    The isolate_mode_t type that appeared in 3.2 was backported to 3.0.39 so
    the version check must be fixed. It was not backported to the 3.1 branch
    though, so it must be excluded.
    
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- probes/lttng-probe-vmscan.c
+++ probes/lttng-probe-vmscan.c
@@ -30,6 +30,8 @@
  */
 #include <trace/events/vmscan.h>
 
+#include "../lttng-kernel-version.h"
+
 /*
  * Create LTTng tracepoint probes.
  */
@@ -37,7 +39,8 @@
 #define CREATE_TRACE_POINTS
 #define TRACE_INCLUDE_PATH ../instrumentation/events/lttng-module
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,2,0))
+#if (LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,38)) || \
+	LTTNG_KERNEL_RANGE(3,1,0, 3,1,10)
 typedef int isolate_mode_t;
 #endif
 
