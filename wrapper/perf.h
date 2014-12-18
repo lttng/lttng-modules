16d7431c3a78605ea598dbd3f081a2b1a8463f23
David Engraf <david.engraf@sysgo.com>
2014-12-18 11:45:22 +0100

    Fix compile error on kernel 3.0.101 with CONFIG_PERF_EVENTS
    The context parameter of perf_event_create_kernel_counter() was introduced
    with kernel 3.1.
    
    Signed-off-by: David Engraf <david.engraf@sysgo.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- wrapper/perf.h
+++ wrapper/perf.h
@@ -23,7 +23,9 @@
 
 #include <linux/perf_event.h>
 
-#if defined(CONFIG_PERF_EVENTS) && (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,99))
+#ifdef CONFIG_PERF_EVENTS
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0))
 static inline struct perf_event *
 wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
 				int cpu,
@@ -32,7 +34,7 @@ wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
 {
 	return perf_event_create_kernel_counter(attr, cpu, task, callback, NULL);
 }
-#else /* defined(CONFIG_PERF_EVENTS) && (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,99)) */
+#else /* (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)) */
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37))
 static inline struct perf_event *
@@ -66,6 +68,8 @@ wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
 #define local64_read(l)		atomic64_read(l)
 #endif
 
-#endif /* defined(CONFIG_PERF_EVENTS) && (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,99)) */
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)) */
+
+#endif /* CONFIG_PERF_EVENTS */
 
 #endif /* _LTTNG_WRAPPER_PERF_H */
