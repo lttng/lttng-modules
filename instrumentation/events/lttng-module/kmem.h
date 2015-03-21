fcce5ffeba665a427341a8765b1153cc4b06675c
Antoine Busque <abusque@efficios.com>
2015-03-21 15:13:18 -0400

    Fix: mm_page_alloc_extfrag instrumentation for kernel 3.14.36
    The following commit has been backported from kernel 3.19.2 into
    3.14.36, introducing a change to mm_page_alloc_extfrag and therefore
    provoking a build failure on a 3.14.36 kernel:
    
    commit a4f3f96f850db69890f47984c60606a2362bee89
    Author: Vlastimil Babka <vbabka@suse.cz>
    Date:   Wed Feb 11 15:28:15 2015 -0800
    
        mm: when stealing freepages, also take pages created by splitting buddy page
    
        commit 99592d598eca62bdbbf62b59941c189176dfc614 upstream.
    
    This patch adds the proper version check to allow building on 3.14.y.
    
    Signed-off-by: Antoine Busque <abusque@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/events/lttng-module/kmem.h
+++ instrumentation/events/lttng-module/kmem.h
@@ -286,7 +286,8 @@ DEFINE_EVENT_PRINT(mm_page, mm_page_pcpu_drain,
 		__entry->order, __entry->migratetype)
 )
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,19,2))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,19,2)	\
+	|| LTTNG_KERNEL_RANGE(3,14,36, 3,15,0))
 
 TRACE_EVENT(mm_page_alloc_extfrag,
 
