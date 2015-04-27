1de8b2e4c021031e72f0f91c1ac984eb8965079e
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-04-27 17:20:28 -0400

    Fix: build x86 kvm probes on kernels >= 4.1-rc1
    iodev.h has moved to include/kvm/.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- probes/lttng-probe-kvm-x86-mmu.c
+++ probes/lttng-probe-kvm-x86-mmu.c
@@ -23,6 +23,13 @@
 #include <linux/module.h>
 #include <linux/kvm_host.h>
 #include "../lttng-tracer.h"
+#include "../lttng-kernel-version.h"
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0))
+#include <kvm/iodev.h>
+#else /* #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0)) */
+#include <../../virt/kvm/iodev.h>
+#endif /* #else #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0)) */
 
 /*
  * Create the tracepoint static inlines from the kernel to validate that our
