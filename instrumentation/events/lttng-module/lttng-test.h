aa3de24db3980016df8ef7dc7c07c2dc2bf6372d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-09-04 16:36:22 -0400

    Implement lttng test module
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ instrumentation/events/lttng-module/lttng-test.h
@@ -0,0 +1,34 @@
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM lttng_test
+
+#if !defined(LTTNG_TRACE_LTTNG_TEST_H) || defined(TRACE_HEADER_MULTI_READ)
+#define LTTNG_TRACE_LTTNG_TEST_H
+
+#include "../../../probes/lttng-tracepoint-event.h"
+#include <linux/types.h>
+#include <linux/version.h>
+
+LTTNG_TRACEPOINT_EVENT(lttng_test_filter_event,
+	TP_PROTO(int anint, int netint, long *values,
+		char *text, size_t textlen,
+		char *etext),
+	TP_ARGS(anint, netint, values, text, textlen, etext),
+	TP_FIELDS(
+		ctf_integer(int, intfield, anint)
+		ctf_integer_hex(int, intfield2, anint)
+		ctf_integer(long, longfield, anint)
+		ctf_integer_network(int, netintfield, netint)
+		ctf_integer_network_hex(int, netintfieldhex, netint)
+		ctf_array(long, arrfield1, values, 3)
+		ctf_array_text(char, arrfield2, text, 10)
+		ctf_sequence(char, seqfield1, text, size_t, textlen)
+		ctf_sequence_text(char, seqfield2, text, size_t, textlen)
+		ctf_string(stringfield, text)
+		ctf_string(stringfield2, etext)
+	)
+)
+
+#endif /*  LTTNG_TRACE_LTTNG_TEST_H */
+
+/* This part must be outside protection */
+#include "../../../probes/define_trace.h"
