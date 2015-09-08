4c6aff7942bea112216f7a19e89ca5e2b6d98395
Lars Persson <lars.persson@axis.com>
2015-09-08 15:20:15 +0200

    Fix: Move pipe override to the arch specific pointers_override.h
    The MIPS architecture has a non-standard calling convention for pipe()
    and cannot use the provided override for the pipe event.
    
    Signed-off-by: Lars Persson <larper@axis.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/syscalls/headers/x86-64-syscalls-3.10.0-rc7_pointers_override.h
+++ instrumentation/syscalls/headers/x86-64-syscalls-3.10.0-rc7_pointers_override.h
@@ -106,6 +106,15 @@ SC_LTTNG_TRACEPOINT_EVENT_CODE(accept,
 	)
 )
 
+#define OVERRIDE_64_pipe
+SC_LTTNG_TRACEPOINT_EVENT(pipe,
+	TP_PROTO(sc_exit(long ret,) int * fildes),
+	TP_ARGS(sc_exit(ret,) fildes),
+	TP_FIELDS(sc_exit(ctf_integer(long, ret, ret))
+		sc_out(ctf_user_array(int, fildes, fildes, 2))
+	)
+)
+
 #else	/* CREATE_SYSCALL_TABLE */
 
 #define OVERRIDE_TABLE_64_clone
