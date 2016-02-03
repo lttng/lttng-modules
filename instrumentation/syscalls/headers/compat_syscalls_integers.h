4c26db898e53165e6484587393a59bdf915f97e4
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-02-03 09:29:15 -0500

    ARM64: wire up compat system calls
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/syscalls/headers/compat_syscalls_integers.h
+++ instrumentation/syscalls/headers/compat_syscalls_integers.h
@@ -1,3 +1,7 @@
 #ifdef CONFIG_X86_64
 #include "x86-32-syscalls-3.1.0-rc6_integers.h"
 #endif
+
+#ifdef CONFIG_ARM64
+#include "arm-32-syscalls-3.4.25_integers.h"
+#endif
--- instrumentation/syscalls/headers/compat_syscalls_pointers.h
+++ instrumentation/syscalls/headers/compat_syscalls_pointers.h
@@ -1,3 +1,7 @@
 #ifdef CONFIG_X86_64
 #include "x86-32-syscalls-3.1.0-rc6_pointers.h"
 #endif
+
+#ifdef CONFIG_ARM64
+#include "arm-32-syscalls-3.4.25_pointers.h"
+#endif
