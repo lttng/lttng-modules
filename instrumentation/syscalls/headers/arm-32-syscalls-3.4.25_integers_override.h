ca956174e7ffe2388e58c05f15a548a758ccfd5a
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-02-22 16:57:37 -0500

    Fix: system call tracing for arm-64 compat !CONFIG_UID16
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/syscalls/headers/arm-32-syscalls-3.4.25_integers_override.h
+++ instrumentation/syscalls/headers/arm-32-syscalls-3.4.25_integers_override.h
@@ -1,4 +1,34 @@
+#ifndef CONFIG_UID16
 
+#define OVERRIDE_32_getuid16
+#define OVERRIDE_32_getgid16
+#define OVERRIDE_32_geteuid16
+#define OVERRIDE_32_getegid16
+#define OVERRIDE_32_setuid16
+#define OVERRIDE_32_setgid16
+#define OVERRIDE_32_setfsuid16
+#define OVERRIDE_32_setfsgid16
+#define OVERRIDE_32_setreuid16
+#define OVERRIDE_32_setregid16
+#define OVERRIDE_32_fchown16
+#define OVERRIDE_32_setresuid16
+#define OVERRIDE_32_setresgid16
+
+#define OVERRIDE_TABLE_32_getuid16
+#define OVERRIDE_TABLE_32_getgid16
+#define OVERRIDE_TABLE_32_geteuid16
+#define OVERRIDE_TABLE_32_getegid16
+#define OVERRIDE_TABLE_32_setuid16
+#define OVERRIDE_TABLE_32_setgid16
+#define OVERRIDE_TABLE_32_setreuid16
+#define OVERRIDE_TABLE_32_setregid16
+#define OVERRIDE_TABLE_32_fchown16
+#define OVERRIDE_TABLE_32_setfsuid16
+#define OVERRIDE_TABLE_32_setfsgid16
+#define OVERRIDE_TABLE_32_setresuid16
+#define OVERRIDE_TABLE_32_setresgid16
+
+#endif
 
 #define OVERRIDE_TABLE_32_arm_fadvise64_64
 #define OVERRIDE_TABLE_32_sync_file_range2
--- instrumentation/syscalls/headers/arm-32-syscalls-3.4.25_pointers_override.h
+++ instrumentation/syscalls/headers/arm-32-syscalls-3.4.25_pointers_override.h
@@ -1,6 +1,14 @@
 
 #define OVERRIDE_TABLE_32_mmap2
 
+# ifndef CONFIG_UID16
+#  define OVERRIDE_32_getgroups16
+#  define OVERRIDE_32_setgroups16
+#  define OVERRIDE_32_lchown16
+#  define OVERRIDE_32_getresuid16
+#  define OVERRIDE_32_getresgid16
+#  define OVERRIDE_32_chown16
+# endif
 
 #ifndef CREATE_SYSCALL_TABLE
 
@@ -28,6 +36,15 @@ SC_LTTNG_TRACEPOINT_EVENT(pipe,
 )
 
 #else	/* CREATE_SYSCALL_TABLE */
+
+# ifndef CONFIG_UID16
+#  define OVERRIDE_TABLE_32_getgroups16
+#  define OVERRIDE_TABLE_32_setgroups16
+#  define OVERRIDE_TABLE_32_lchown16
+#  define OVERRIDE_TABLE_32_getresuid16
+#  define OVERRIDE_TABLE_32_getresgid16
+#  define OVERRIDE_TABLE_32_chown16
+# endif
 
 #define OVERRIDE_TABLE_32_execve
 TRACE_SYSCALL_TABLE(execve, execve, 11, 3)
