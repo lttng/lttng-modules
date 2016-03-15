2def77ecf53f19e10f6f9ca4c7d71be023c625ee
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-03-15 10:59:47 -0400

    Use system include path in probes/define_trace.h
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- probes/define_trace.h
+++ probes/define_trace.h
@@ -97,7 +97,7 @@
 #define DECLARE_TRACE(name, proto, args)
 
 #ifdef LTTNG_PACKAGE_BUILD
-#include "lttng-tracepoint-event-impl.h"
+#include <probes/lttng-tracepoint-event-impl.h>
 #endif
 
 #undef LTTNG_TRACEPOINT_EVENT
