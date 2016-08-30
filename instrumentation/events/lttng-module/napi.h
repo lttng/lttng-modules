c1dacd91afb9874948d9e469496620a1aa27f0a9
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-08-30 17:02:09 -0400

    Fix: napi instrumentation for 4.8 kernels
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/events/lttng-module/napi.h
+++ instrumentation/events/lttng-module/napi.h
@@ -10,6 +10,24 @@
 
 #define NO_DEV "(no_device)"
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0))
+
+LTTNG_TRACEPOINT_EVENT(napi_poll,
+
+	TP_PROTO(struct napi_struct *napi, int work, int budget),
+
+	TP_ARGS(napi, work, budget),
+
+	TP_FIELDS(
+		ctf_integer_hex(struct napi_struct *, napi, napi)
+		ctf_string(dev_name, napi->dev ? napi->dev->name : NO_DEV)
+		ctf_integer(int, work, work)
+		ctf_integer(int, budget, budget)
+	)
+)
+
+#else
+
 LTTNG_TRACEPOINT_EVENT(napi_poll,
 
 	TP_PROTO(struct napi_struct *napi),
@@ -21,6 +39,8 @@ LTTNG_TRACEPOINT_EVENT(napi_poll,
 		ctf_string(dev_name, napi->dev ? napi->dev->name : NO_DEV)
 	)
 )
+
+#endif
 
 #undef NO_DEV
 
