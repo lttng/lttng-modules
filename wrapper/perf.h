6f845adbb0e3af6b855a283474dc1334bcb69d06
Michael Jeanson <mjeanson@efficios.com>
2018-11-08 15:27:19 -0500

    Drop support for kernels < 3.0 from perf.h wrapper
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>

--- wrapper/perf.h
+++ wrapper/perf.h
@@ -22,8 +22,6 @@ wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
 	return perf_event_create_kernel_counter(attr, cpu, task, callback, NULL);
 }
 #else /* (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)) */
-
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37))
 static inline struct perf_event *
 wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
 				int cpu,
@@ -32,29 +30,6 @@ wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
 {
 	return perf_event_create_kernel_counter(attr, cpu, task, callback);
 }
-#else /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)) */
-static inline struct perf_event *
-wrapper_perf_event_create_kernel_counter(struct perf_event_attr *attr,
-				int cpu,
-				struct task_struct *task,
-				perf_overflow_handler_t callback)
-{
-	pid_t pid;
-
-	if (!task)
-		pid = -1;
-	else
-		pid = task->pid;
-
-	return perf_event_create_kernel_counter(attr, cpu, pid, callback);
-}
-
-#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)) */
-
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36))
-#define local64_read(l)		atomic64_read(l)
-#endif
-
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)) */
 
 #endif /* CONFIG_PERF_EVENTS */
