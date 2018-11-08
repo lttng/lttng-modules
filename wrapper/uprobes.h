8134eb60252cc167be0306bfb3780c4507eb0d16
Michael Jeanson <mjeanson@efficios.com>
2018-11-08 18:37:45 -0500

    Add missing SPDX license identifiers to uprobes
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- wrapper/uprobes.h
+++ wrapper/uprobes.h
@@ -1,7 +1,5 @@
-#ifndef _LTTNG_WRAPPER_UPROBES_H
-#define _LTTNG_WRAPPER_UPROBES_H
-
-/*
+/* SPDX-License-Identifier: (GPL-2.0 or LGPL-2.1)
+ *
  * wrapper/uprobes.h
  *
  * wrapper around uprobes. Using KALLSYMS to get its address when
@@ -11,20 +9,10 @@
  * Copyright (C) 2013 Yannick Brosseau <yannick.brosseau@gmail.com>
  * Copyright (C) 2017 Francis Deslauriers <francis.deslauriers@efficios.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; only
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
+#ifndef _LTTNG_WRAPPER_UPROBES_H
+#define _LTTNG_WRAPPER_UPROBES_H
 
 #include <linux/version.h>
 
