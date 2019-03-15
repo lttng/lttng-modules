80f8783b611cc11e5cad9ad40173b4ee3538ca1c
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2019-03-15 11:13:39 -0400

    Fix: extra-version-git.sh redirect stderr to /dev/null
    Running make in a git repo that does not contain any tag prints:
    
    fatal: No names found, cannot describe anything.
    
    in the make and make clean outputs.
    
    It's fine to have no tag name available (extra-version-git.sh will
    return the value 0), but we should not print an error in the make
    output. Redirect this error to /dev/null.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Suggested-by: Michael Jeanson <mjeanson@efficios.com>

--- extra-version-git.sh
+++ extra-version-git.sh
@@ -7,7 +7,7 @@ TOP_LTTNG_MODULES_DIR="$1"
 GIT_VERSION="0"
 
 if test -x "$(which git 2>&1;true)" && test -r "${TOP_LTTNG_MODULES_DIR}/.git"; then
-	GIT_VERSION_STR="$(cd "${TOP_LTTNG_MODULES_DIR}" && git describe --tags --dirty)"
+	GIT_VERSION_STR="$(cd "${TOP_LTTNG_MODULES_DIR}" && git describe --tags --dirty 2> /dev/null)"
 	GIT_CURRENT_TAG="$(cd "${TOP_LTTNG_MODULES_DIR}" && git describe --tags --exact-match --match="v[0-9]*" HEAD 2> /dev/null)"
 
 	GIT_VERSION="${GIT_VERSION_STR}"
