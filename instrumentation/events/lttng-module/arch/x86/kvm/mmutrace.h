8a88382fb09bbeda443044ee8cdb8f92040636bc
Michael Jeanson <mjeanson@efficios.com>
2019-03-18 16:20:35 -0400

    Fix: Revert "KVM: MMU: show mmu_valid_gen..." (v5.1)
    See upstream commit :
    
      commit b59c4830ca185ba0e9f9e046fb1cd10a4a92627a
      Author: Sean Christopherson <sean.j.christopherson@intel.com>
      Date:   Tue Feb 5 13:01:30 2019 -0800
    
        Revert "KVM: MMU: show mmu_valid_gen in shadow page related tracepoints"
    
        ...as part of removing x86 KVM's fast invalidate mechanism, i.e. this
        is one part of a revert all patches from the series that introduced the
        mechanism[1].
    
        This reverts commit 2248b023219251908aedda0621251cffc548f258.
    
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/events/lttng-module/arch/x86/kvm/mmutrace.h
+++ instrumentation/events/lttng-module/arch/x86/kvm/mmutrace.h
@@ -14,7 +14,15 @@
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM kvm_mmu
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,11,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,1,0))
+
+#define LTTNG_KVM_MMU_PAGE_FIELDS \
+	ctf_integer(__u64, gfn, (sp)->gfn) \
+	ctf_integer(__u32, role, (sp)->role.word) \
+	ctf_integer(__u32, root_count, (sp)->root_count) \
+	ctf_integer(bool, unsync, (sp)->unsync)
+
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3,11,0))
 
 #define LTTNG_KVM_MMU_PAGE_FIELDS \
 	ctf_integer(unsigned long, mmu_valid_gen, (sp)->mmu_valid_gen) \
