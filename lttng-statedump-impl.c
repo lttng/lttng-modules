dd970ee8196b857ce89278a55b7ac471593dc95d
Michael Jeanson <mjeanson@efficios.com>
2020-02-24 15:15:36 -0500

    fix: move lttng_close_on_exec to proper wrapper
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I330c6d63fc9c1af00c9a9471563d322d036b9a9c

--- lttng-statedump-impl.c
+++ lttng-statedump-impl.c
@@ -41,7 +41,6 @@
 #include <linux/inetdevice.h>
 #include <linux/sched.h>
 #include <linux/mm.h>
-#include <linux/fdtable.h>
 #include <linux/swap.h>
 #include <linux/wait.h>
 #include <linux/mutex.h>
@@ -56,7 +55,7 @@
 #include <wrapper/tracepoint.h>
 #include <wrapper/genhd.h>
 #include <wrapper/file.h>
-#include <wrapper/time.h>
+#include <wrapper/fdtable.h>
 
 #ifdef CONFIG_LTTNG_HAS_LIST_IRQ
 #include <linux/irq.h>
