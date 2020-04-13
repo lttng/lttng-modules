a3e70c0e6b666502c587fbfb996e72337cda04b9
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2020-04-13 16:36:27 -0400

    instrumentation: v4l2: remove compatibility code

--- instrumentation/events/lttng-module/v4l2.h
+++ instrumentation/events/lttng-module/v4l2.h
@@ -7,7 +7,6 @@
 
 #include <probes/lttng-tracepoint-event.h>
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0))
 LTTNG_TRACEPOINT_EVENT_CLASS(v4l2_class,
 
 	TP_PROTO(int minor, struct v4l2_buffer *buf),
@@ -32,32 +31,6 @@ LTTNG_TRACEPOINT_EVENT_CLASS(v4l2_class,
 		ctf_integer(u32, sequence, buf->sequence)
 	)
 )
-#else
-LTTNG_TRACEPOINT_EVENT_CLASS(v4l2_class,
-
-	TP_PROTO(int minor, struct v4l2_buffer *buf),
-
-	TP_ARGS(minor, buf),
-
-	TP_FIELDS(
-		ctf_integer(int, minor, minor)
-		ctf_integer(u32, index, buf->index)
-		ctf_integer(u32, type, buf->type)
-		ctf_integer(u32, bytesused, buf->bytesused)
-		ctf_integer(u32, flags, buf->flags)
-		ctf_integer(u32, field, buf->field)
-		ctf_integer(s64, timestamp, timeval_to_ns(&buf->timestamp))
-		ctf_integer(u32, timecode_type, buf->timecode.type)
-		ctf_integer(u32, timecode_flags, buf->timecode.flags)
-		ctf_integer(u8, timecode_frames, buf->timecode.frames)
-		ctf_integer(u8, timecode_seconds, buf->timecode.seconds)
-		ctf_integer(u8, timecode_minutes, buf->timecode.minutes)
-		ctf_integer(u8, timecode_hours, buf->timecode.hours)
-		ctf_array(u8, timecode_userbits, buf->timecode.userbits, 4)
-		ctf_integer(u32, sequence, buf->sequence)
-	)
-)
-#endif
 
 LTTNG_TRACEPOINT_EVENT_INSTANCE(v4l2_class,
 	v4l2_dqbuf,
