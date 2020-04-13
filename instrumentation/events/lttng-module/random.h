2eb70d5681beb2b9739b071ef93b5e7efbf69428
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2020-04-13 16:23:57 -0400

    instrumentation: random: remove compatibility code

--- instrumentation/events/lttng-module/random.h
+++ instrumentation/events/lttng-module/random.h
@@ -38,7 +38,6 @@ LTTNG_TRACEPOINT_EVENT_INSTANCE_MAP(random__mix_pool_bytes, mix_pool_bytes_noloc
 	TP_ARGS(pool_name, bytes, IP)
 )
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,2,0))
 LTTNG_TRACEPOINT_EVENT_MAP(credit_entropy_bits,
 
 	random_credit_entropy_bits,
@@ -55,25 +54,6 @@ LTTNG_TRACEPOINT_EVENT_MAP(credit_entropy_bits,
 		ctf_integer_hex(unsigned long, IP, IP)
 	)
 )
-#else
-LTTNG_TRACEPOINT_EVENT_MAP(credit_entropy_bits,
-
-	random_credit_entropy_bits,
-
-	TP_PROTO(const char *pool_name, int bits, int entropy_count,
-		 int entropy_total, unsigned long IP),
-
-	TP_ARGS(pool_name, bits, entropy_count, entropy_total, IP),
-
-	TP_FIELDS(
-		ctf_string(pool_name, pool_name)
-		ctf_integer(int, bits, bits)
-		ctf_integer(int, entropy_count, entropy_count)
-		ctf_integer(int, entropy_total, entropy_total)
-		ctf_integer_hex(unsigned long, IP, IP)
-	)
-)
-#endif
 
 LTTNG_TRACEPOINT_EVENT_MAP(get_random_bytes,
 
