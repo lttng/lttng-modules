cfc242c8df5692183d79e2c3022d3bbb6eaae7e0
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2020-04-13 16:34:55 -0400

    instrumentation: sock: remove compatibility code

--- instrumentation/events/lttng-module/sock.h
+++ instrumentation/events/lttng-module/sock.h
@@ -6,7 +6,6 @@
 #define LTTNG_TRACE_SOCK_H
 
 #include <probes/lttng-tracepoint-event.h>
-#include <linux/version.h>
 #include <net/sock.h>
 
 LTTNG_TRACEPOINT_EVENT(sock_rcvqueue_full,
@@ -21,8 +20,6 @@ LTTNG_TRACEPOINT_EVENT(sock_rcvqueue_full,
 		ctf_integer(int, sk_rcvbuf, sk->sk_rcvbuf)
 	)
 )
-
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,19,0))
 
 LTTNG_TRACEPOINT_EVENT(sock_exceed_buf_limit,
 
@@ -42,42 +39,6 @@ LTTNG_TRACEPOINT_EVENT(sock_exceed_buf_limit,
 		ctf_integer(int, kind, kind)
 	)
 )
-
-#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0))
-
-LTTNG_TRACEPOINT_EVENT(sock_exceed_buf_limit,
-
-	TP_PROTO(struct sock *sk, struct proto *prot, long allocated),
-
-	TP_ARGS(sk, prot, allocated),
-
-	TP_FIELDS(
-		ctf_string(name, prot->name)
-		ctf_array(long, sysctl_mem, prot->sysctl_mem, 3)
-		ctf_integer(long, allocated, allocated)
-		ctf_integer(int, sysctl_rmem, sk_get_rmem0(sk, prot))
-		ctf_integer(int, rmem_alloc, atomic_read(&sk->sk_rmem_alloc))
-	)
-)
-
-#else /* #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0)) */
-
-LTTNG_TRACEPOINT_EVENT(sock_exceed_buf_limit,
-
-	TP_PROTO(struct sock *sk, struct proto *prot, long allocated),
-
-	TP_ARGS(sk, prot, allocated),
-
-	TP_FIELDS(
-		ctf_string(name, prot->name)
-		ctf_array(long, sysctl_mem, prot->sysctl_mem, 3)
-		ctf_integer(long, allocated, allocated)
-		ctf_integer(int, sysctl_rmem, prot->sysctl_rmem[0])
-		ctf_integer(int, rmem_alloc, atomic_read(&sk->sk_rmem_alloc))
-	)
-)
-
-#endif /* #else #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0)) */
 
 #endif /* LTTNG_TRACE_SOCK_H */
 
