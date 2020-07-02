ff04d185d2142f88a01bc8ff17ca2b61ab4622b7
Michael Jeanson <mjeanson@efficios.com>
2020-07-02 12:06:42 -0400

    Use exported symbol bdevname() instead of disk_name()
    bdevname() is a simple wrapper over disk_name() but has the honor to be
    exported. Using it removes the need for a kallsym wrapper.
    
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ic2b2233c4db7826175c68edea69751ddcb17a5e6

--- include/wrapper/genhd.h
+++ include/wrapper/genhd.h
@@ -14,36 +14,10 @@
 
 #include <linux/genhd.h>
 
-#ifdef CONFIG_KALLSYMS
+#ifdef CONFIG_KALLSYMS_ALL
 
 #include <linux/kallsyms.h>
 #include <wrapper/kallsyms.h>
-
-static inline
-char *wrapper_disk_name(struct gendisk *hd, int partno, char *buf)
-{
-	char *(*disk_name_sym)(struct gendisk *hd, int partno, char *buf);
-
-	disk_name_sym = (void *) kallsyms_lookup_funcptr("disk_name");
-	if (disk_name_sym) {
-		return disk_name_sym(hd, partno, buf);
-	} else {
-		printk_once(KERN_WARNING "LTTng: disk_name symbol lookup failed.\n");
-		return NULL;
-	}
-}
-
-#else
-
-static inline
-char *wrapper_disk_name(struct gendisk *hd, int partno, char *buf)
-{
-	return disk_name(hd, partno, buf);
-}
-
-#endif
-
-#ifdef CONFIG_KALLSYMS_ALL
 
 static inline
 struct class *wrapper_get_block_class(void)
