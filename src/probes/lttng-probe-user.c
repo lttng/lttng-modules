eb94dcd91d4ea827ebe0353249e309f1629a5032
Michael Jeanson <mjeanson@efficios.com>
2020-09-25 11:23:58 -0400

    fix: Add a 1MB limit to lttng_strlen_user_inatomic
    The previous implementation was unbounded which could result in long
    loops with preemption turned off.
    
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I85afcd879258735bb2e7502f6016fcb2d3974cf7

--- src/probes/lttng-probe-user.c
+++ src/probes/lttng-probe-user.c
@@ -10,6 +10,8 @@
 #include <wrapper/uaccess.h>
 #include <lttng/probe-user.h>
 
+#define LTTNG_MAX_USER_STRING_LEN 1048576 /* 1MB */
+
 /*
  * Calculate string length. Include final null terminating character if there is
  * one, or ends at first fault. Disabling page faults ensures that we can safely
@@ -41,6 +43,8 @@ long lttng_strlen_user_inatomic(const char *addr)
 		if (unlikely(ret > 0))
 			break;
 		count++;
+		if (unlikely(count > LTTNG_MAX_USER_STRING_LEN))
+			break;
 		if (unlikely(!v))
 			break;
 		addr++;
