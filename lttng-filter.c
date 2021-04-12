lttng/lttng-modules

commit 5f7779c5265d15bd087fde0b2694249e11ea9472

Fix: bytecode linker: validate event and field array/sequence encoding
The bytecode linker should only allow linking filter expressions loading
fields which are string-encoded arrays and sequence for comparison
against a string, and reject arrays and sequences without encoding, so
the filter interpreter does not attempt to load non-NULL terminated
arrays/sequences as if they were strings.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Change-Id: I61213b736b2e41b55ad8d6b32a6db0f50494e316

lttng-filter.c
@@ -241,12 +241,29 @@ int apply_field_reloc(struct lttng_event *event,
 			op->op = FILTER_OP_LOAD_FIELD_REF_S64;
 			break;
 		case atype_array:
-		case atype_sequence:
+		{
+			const struct lttng_basic_type *elem_type = &field->type.u.array.elem_type;
+
+			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+				return -EINVAL;
 			if (field->user)
 				op->op = FILTER_OP_LOAD_FIELD_REF_USER_SEQUENCE;
 			else
 				op->op = FILTER_OP_LOAD_FIELD_REF_SEQUENCE;
 			break;
+		}
+		case atype_sequence:
+		{
+			const struct lttng_basic_type *elem_type = &field->type.u.sequence.elem_type;
+
+			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+				return -EINVAL;
+			if (field->user)
+				op->op = FILTER_OP_LOAD_FIELD_REF_USER_SEQUENCE;
+			else
+				op->op = FILTER_OP_LOAD_FIELD_REF_SEQUENCE;
+			break;
+		}
 		case atype_string:
 			if (field->user)
 				op->op = FILTER_OP_LOAD_FIELD_REF_USER_STRING;
@@ -311,9 +328,27 @@ int apply_context_reloc(struct lttng_event *event,
 			op->op = FILTER_OP_GET_CONTEXT_REF_S64;
 			break;
 			/* Sequence and array supported as string */
-		case atype_string:
 		case atype_array:
+		{
+			const struct lttng_basic_type *elem_type = &ctx_field->event_field.type.u.array.elem_type;
+
+			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+				return -EINVAL;
+			BUG_ON(ctx_field->event_field.user);
+			op->op = FILTER_OP_GET_CONTEXT_REF_STRING;
+			break;
+		}
 		case atype_sequence:
+		{
+			const struct lttng_basic_type *elem_type = &ctx_field->event_field.type.u.sequence.elem_type;
+
+			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+				return -EINVAL;
+			BUG_ON(ctx_field->event_field.user);
+			op->op = FILTER_OP_GET_CONTEXT_REF_STRING;
+			break;
+		}
+		case atype_string:
 			BUG_ON(ctx_field->event_field.user);
 			op->op = FILTER_OP_GET_CONTEXT_REF_STRING;
 			break;

commit 8e2637068252be8940b7f99a61e86b1bd91df95a

Fix: properly compare type enumeration
Fixes: 5f7779c5265d ("Fix: bytecode linker: validate event and field array/sequence encoding")
Fixes: #1304
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Change-Id: I9b5e3df5415d56420365997ce289be7e02a0140e

lttng-filter.c
@@ -244,7 +244,7 @@ int apply_field_reloc(struct lttng_event *event,
 		{
 			const struct lttng_basic_type *elem_type = &field->type.u.array.elem_type;
 
-			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+			if (elem_type->atype != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
 				return -EINVAL;
 			if (field->user)
 				op->op = FILTER_OP_LOAD_FIELD_REF_USER_SEQUENCE;
@@ -256,7 +256,7 @@ int apply_field_reloc(struct lttng_event *event,
 		{
 			const struct lttng_basic_type *elem_type = &field->type.u.sequence.elem_type;
 
-			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+			if (elem_type->atype != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
 				return -EINVAL;
 			if (field->user)
 				op->op = FILTER_OP_LOAD_FIELD_REF_USER_SEQUENCE;
@@ -332,7 +332,7 @@ int apply_context_reloc(struct lttng_event *event,
 		{
 			const struct lttng_basic_type *elem_type = &ctx_field->event_field.type.u.array.elem_type;
 
-			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+			if (elem_type->atype != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
 				return -EINVAL;
 			BUG_ON(ctx_field->event_field.user);
 			op->op = FILTER_OP_GET_CONTEXT_REF_STRING;
@@ -342,7 +342,7 @@ int apply_context_reloc(struct lttng_event *event,
 		{
 			const struct lttng_basic_type *elem_type = &ctx_field->event_field.type.u.sequence.elem_type;
 
-			if (elem_type != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
+			if (elem_type->atype != atype_integer || elem_type->u.basic.integer.encoding == lttng_encode_none)
 				return -EINVAL;
 			BUG_ON(ctx_field->event_field.user);
 			op->op = FILTER_OP_GET_CONTEXT_REF_STRING;