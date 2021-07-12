62c3d34c566d1ec4fee6843950f078e7d79afbe9
Michael Jeanson <mjeanson@efficios.com>
2021-07-12 14:51:20 -0400

    fix: sched: Change task_struct::state (v5.14)
    See upstream commit:
    
      commit 2f064a59a11ff9bc22e52e9678bc601404c7cb34
      Author: Peter Zijlstra <peterz@infradead.org>
      Date:   Fri Jun 11 10:28:17 2021 +0200
    
        sched: Change task_struct::state
    
        Change the type and name of task_struct::state. Drop the volatile and
        shrink it to an 'unsigned int'. Rename it in order to find all uses
        such that we can use READ_ONCE/WRITE_ONCE as appropriate.
    
    Change-Id: I3a379192d6b977753fe58d4f67833a78dd7a0a47
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ wrapper/sched.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: (GPL-2.0-only or LGPL-2.1-only)
+ *
+ * src/wrapper/kprobes.h
+ *
+ * Copyright (C) 2021 Michael Jeanson <mjeanson@efficios.com>
+ */
+
+#ifndef _LTTNG_WRAPPER_SCHED_H
+#define _LTTNG_WRAPPER_SCHED_H
+
+#include <linux/sched.h>
+#include <lttng/kernel-version.h>
+
+#if (LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(5,14,0))
+
+#define lttng_get_task_state(task)	READ_ONCE((task)->__state)
+#define lttng_task_is_running(task)	task_is_running(task)
+
+#else /* LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(5,14,0) */
+
+#define lttng_get_task_state(task)	((task)->state)
+#define lttng_task_is_running(task)	(lttng_get_task_state(task) == TASK_RUNNING)
+
+#endif /* LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(5,14,0) */
+
+#endif /* _LTTNG_WRAPPER_SCHED_H */
