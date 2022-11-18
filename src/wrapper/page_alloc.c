5a90857e90423e5849fafc875ff1eacc6f56d5ac
Michael Jeanson <mjeanson@efficios.com>
2022-11-18 16:34:33 -0500

    Drop support for kernels < 4.4 from 'wrapper/page_alloc.h'
    Change-Id: I1a702b78a5293ce631a7955fe67145fdf7445cb6
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/wrapper/page_alloc.c
+++ src/wrapper/page_alloc.c
@@ -9,14 +9,7 @@
  * Copyright (C) 2015 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
  */
 
-#include <lttng/kernel-version.h>
-
-#if (defined(CONFIG_KALLSYMS) \
-	&& (LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(3,19,2)	\
-		|| LTTNG_KERNEL_RANGE(3,14,36, 3,15,0)		\
-		|| LTTNG_KERNEL_RANGE(3,18,10, 3,19,0)		\
-		|| LTTNG_DEBIAN_KERNEL_RANGE(3,16,7,9,0,0, 3,17,0,0,0,0) \
-		|| LTTNG_UBUNTU_KERNEL_RANGE(3,16,7,34, 3,17,0,0)))
+#ifdef CONFIG_KALLSYMS
 
 #include <linux/kallsyms.h>
 #include <linux/mm_types.h>
@@ -72,49 +65,6 @@ unsigned long __canary__get_pfnblock_flags_mask(struct page *page,
 {
 	return get_pfnblock_flags_mask(page, pfn, end_bitidx, mask);
 }
-
-#else
-
-#include <linux/pageblock-flags.h>
-
-#endif
-
-#if (defined(CONFIG_KALLSYMS) \
-	&& LTTNG_UBUNTU_KERNEL_RANGE(3,13,11,50, 3,14,0,0))
-
-#include <linux/kallsyms.h>
-#include <linux/mm_types.h>
-#include <linux/module.h>
-#include <wrapper/kallsyms.h>
-#include <wrapper/page_alloc.h>
-
-static
-unsigned long (*get_pageblock_flags_mask_sym)(struct page *page,
-		unsigned long end_bitidx,
-		unsigned long mask);
-
-unsigned long wrapper_get_pageblock_flags_mask(struct page *page,
-		unsigned long end_bitidx,
-		unsigned long mask)
-{
-	WARN_ON_ONCE(!get_pageblock_flags_mask_sym);
-	if (get_pageblock_flags_mask_sym) {
-		return get_pageblock_flags_mask_sym(page, end_bitidx, mask);
-	} else {
-		return -ENOSYS;
-	}
-}
-EXPORT_SYMBOL_GPL(wrapper_get_pageblock_flags_mask);
-
-int wrapper_get_pageblock_flags_mask_init(void)
-{
-	get_pageblock_flags_mask_sym =
-		(void *) kallsyms_lookup_funcptr("get_pageblock_flags_mask");
-	if (!get_pageblock_flags_mask_sym)
-		return -1;
-	return 0;
-}
-EXPORT_SYMBOL_GPL(wrapper_get_pfnblock_flags_mask_init);
 
 #else
 
