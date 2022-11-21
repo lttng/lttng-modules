861292216239a98feb4eba0fe9777f0dfcd6c48a
Michael Jeanson <mjeanson@efficios.com>
2022-11-21 13:38:52 -0500

    Cleanup duplicated include in wrapper/timer.h
    Change-Id: I7a2682d4a584954118fc110a1e374a107c4361ab
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/wrapper/timer.h
+++ include/wrapper/timer.h
@@ -12,7 +12,6 @@
 
 #include <lttng/kernel-version.h>
 #include <linux/timer.h>
-#include <lttng/kernel-version.h>
 
 /*
  * In the olden days, pinned timers were initialized normaly with init_timer()
