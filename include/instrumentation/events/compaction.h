441e0d1623cd5d8a192099fbbc9be8c2ecb9ae2d
Michael Jeanson <mjeanson@efficios.com>
2022-11-21 11:39:21 -0500

    Drop support for kernels < 4.4 from compaction instrumentation
    Change-Id: Ic9305d3860ff3951f08e15e25d2a2f2bd0ef7913
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/instrumentation/events/compaction.h
+++ include/instrumentation/events/compaction.h
@@ -9,8 +9,6 @@
 #include <linux/types.h>
 #include <lttng/kernel-version.h>
 
-#if LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(4,0,0)
-
 LTTNG_TRACEPOINT_EVENT_CLASS(compaction_isolate_template,
 
 	TP_PROTO(unsigned long start_pfn,
@@ -56,47 +54,6 @@ LTTNG_TRACEPOINT_EVENT_INSTANCE_MAP(compaction_isolate_template,
 	TP_ARGS(start_pfn, end_pfn, nr_scanned, nr_taken)
 )
 
-#else /* #if LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(4,0,0) */
-
-LTTNG_TRACEPOINT_EVENT_CLASS(compaction_isolate_template,
-
-	TP_PROTO(unsigned long nr_scanned,
-		unsigned long nr_taken),
-
-	TP_ARGS(nr_scanned, nr_taken),
-
-	TP_FIELDS(
-		ctf_integer(unsigned long, nr_scanned, nr_scanned)
-		ctf_integer(unsigned long, nr_taken, nr_taken)
-	)
-)
-
-LTTNG_TRACEPOINT_EVENT_INSTANCE_MAP(compaction_isolate_template,
-
-	mm_compaction_isolate_migratepages,
-
-	compaction_isolate_migratepages,
-
-	TP_PROTO(unsigned long nr_scanned,
-		unsigned long nr_taken),
-
-	TP_ARGS(nr_scanned, nr_taken)
-)
-
-LTTNG_TRACEPOINT_EVENT_INSTANCE_MAP(compaction_isolate_template,
-
-	mm_compaction_isolate_freepages,
-
-	compaction_isolate_freepages,
-
-	TP_PROTO(unsigned long nr_scanned,
-		unsigned long nr_taken),
-
-	TP_ARGS(nr_scanned, nr_taken)
-)
-
-#endif /* #else #if LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(4,0,0) */
-
 #if (LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(5,18,0))
 LTTNG_TRACEPOINT_EVENT_MAP(mm_compaction_migratepages,
 
@@ -127,9 +84,7 @@ LTTNG_TRACEPOINT_EVENT_MAP(mm_compaction_migratepages,
 		ctf_integer(unsigned long, nr_failed, nr_all - nr_succeeded)
 	)
 )
-#elif LTTNG_KERNEL_RANGE(3,12,30, 3,13,0) || \
-	LTTNG_KERNEL_RANGE(3,14,25, 3,15,0) || \
-	(LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(3,16,0))
+#else
 LTTNG_TRACEPOINT_EVENT_CODE_MAP(mm_compaction_migratepages,
 
 	compaction_migratepages,
@@ -164,21 +119,6 @@ LTTNG_TRACEPOINT_EVENT_CODE_MAP(mm_compaction_migratepages,
 	),
 
 	TP_code_post()
-)
-#else /* #if (LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(3,16,0)) */
-LTTNG_TRACEPOINT_EVENT_MAP(mm_compaction_migratepages,
-
-	compaction_migratepages,
-
-	TP_PROTO(unsigned long nr_migrated,
-		unsigned long nr_failed),
-
-	TP_ARGS(nr_migrated, nr_failed),
-
-	TP_FIELDS(
-		ctf_integer(unsigned long, nr_migrated, nr_migrated)
-		ctf_integer(unsigned long, nr_failed, nr_failed)
-	)
 )
 #endif /* #else #if (LTTNG_LINUX_VERSION_CODE >= LTTNG_KERNEL_VERSION(3,16,0)) */
 
