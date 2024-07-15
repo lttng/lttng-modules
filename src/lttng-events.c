compudj/lttng-modules-dev

Implement event counter creation
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Change-Id: I5b407513c5c64ae418fc1846ae10f3fa99609796

src/lttng-events.c
@@ -2386,6 +2386,62 @@ int lttng_fix_pending_event_notifiers(void)
 	return 0;
 }
 
+static
+int copy_counter_key(struct lttng_counter_key *key,
+		     const struct lttng_kernel_abi_counter_key *key_param)
+{
+	size_t i, j, nr_dimensions;
+
+	nr_dimensions = key_param->nr_dimensions;
+	if (nr_dimensions > LTTNG_KERNEL_ABI_COUNTER_DIMENSION_MAX)
+		return -EINVAL;
+	key->nr_dimensions = nr_dimensions;
+	for (i = 0; i < nr_dimensions; i++) {
+		const struct lttng_kernel_abi_counter_key_dimension *udim =
+			&key_param->key_dimensions[i];
+		struct lttng_counter_key_dimension *dim =
+			&key->key_dimensions[i];
+		size_t nr_key_tokens;
+
+		nr_key_tokens = udim->nr_key_tokens;
+		if (!nr_key_tokens || nr_key_tokens > LTTNG_KERNEL_ABI_NR_KEY_TOKEN)
+			return -EINVAL;
+		dim->nr_key_tokens = nr_key_tokens;
+		for (j = 0; j < nr_key_tokens; j++) {
+			const struct lttng_kernel_abi_key_token *utoken =
+				&udim->key_tokens[j];
+			struct lttng_key_token *token =
+				&dim->key_tokens[j];
+
+			switch (utoken->type) {
+			case LTTNG_KERNEL_ABI_KEY_TOKEN_STRING:
+			{
+				long ret;
+
+				token->type = LTTNG_KERNEL_ABI_KEY_TOKEN_STRING;
+				ret = strncpy_from_user(token->arg.string,
+					(char __user *)(unsigned long)utoken->arg.string_ptr,
+					LTTNG_KERNEL_ABI_KEY_TOKEN_STRING_LEN_MAX);
+				if (ret < 0)
+					return -EFAULT;
+				if (!ret || ret == LTTNG_KERNEL_ABI_KEY_TOKEN_STRING_LEN_MAX)
+					return -EINVAL;
+				break;
+			}
+			case LTTNG_KERNEL_ABI_KEY_TOKEN_EVENT_NAME:
+				token->type = LTTNG_KERNEL_ABI_KEY_TOKEN_EVENT_NAME;
+				break;
+			case LTTNG_KERNEL_ABI_KEY_TOKEN_PROVIDER_NAME:
+				printk(KERN_ERR "LTTng: Provider name token not supported.\n");
+				lttng_fallthrough;
+			default:
+				return -EINVAL;
+			}
+		}
+	}
+	return 0;
+}
+
 struct lttng_event_recorder_enabler *lttng_event_recorder_enabler_create(
 		enum lttng_enabler_format_type format_type,
 		struct lttng_kernel_abi_event *event_param,
@@ -2409,12 +2465,43 @@ struct lttng_event_recorder_enabler *lttng_event_recorder_enabler_create(
 	return event_enabler;
 }
 
+struct lttng_event_counter_enabler *lttng_event_counter_enabler_create(
+		enum lttng_enabler_format_type format_type,
+		struct lttng_kernel_abi_event *event_param,
+		const struct lttng_kernel_abi_counter_key *key,
+		struct lttng_kernel_channel_counter *chan)
+{
+	struct lttng_event_counter_enabler *event_enabler;
+
+	event_enabler = kzalloc(sizeof(*event_enabler), GFP_KERNEL);
+	if (!event_enabler)
+		return NULL;
+	event_enabler->parent.parent.enabler_type = LTTNG_EVENT_ENABLER_TYPE_COUNTER;
+	event_enabler->parent.parent.format_type = format_type;
+	INIT_LIST_HEAD(&event_enabler->parent.parent.filter_bytecode_head);
+	memcpy(&event_enabler->parent.parent.event_param, event_param,
+		sizeof(event_enabler->parent.parent.event_param));
+	event_enabler->chan = chan;
+	event_enabler->parent.chan = &chan->parent;
+	if (key) {
+		if (copy_counter_key(&event_enabler->key, key)) {
+			kfree(event_enabler);
+			return NULL;
+		}
+	}
+
+	/* ctx left NULL */
+	event_enabler->parent.parent.enabled = 0;
+	event_enabler->parent.parent.user_token = event_param->token;
+	return event_enabler;
+}
+
 void lttng_event_enabler_session_add(struct lttng_kernel_session *session,
-		struct lttng_event_recorder_enabler *event_enabler)
+		struct lttng_event_enabler_session_common *event_enabler)
 {
 	mutex_lock(&sessions_mutex);
-	list_add(&event_enabler->parent.parent.node, &session->priv->enablers_head);
-	event_enabler->parent.parent.published = true;
+	list_add(&event_enabler->parent.node, &session->priv->enablers_head);
+	event_enabler->parent.published = true;
 	lttng_session_lazy_sync_event_enablers(session);
 	mutex_unlock(&sessions_mutex);
 }

lttng/lttng-modules

src/lttng-events.c
@@ -1421,10 +1421,7 @@ struct lttng_kernel_event_common *_lttng_kernel_event_create(struct lttng_event_
 	}
 
 	case LTTNG_KERNEL_ABI_SYSCALL:
-		/*
-		 * Needs to be explicitly enabled after creation, since
-		 * we may want to apply filters.
-		 */
+		/* Event will be enabled by enabler sync. */
 		event->enabled = 0;
 		event->priv->registered = 0;
 		event->priv->desc = event_desc;
@@ -2371,6 +2368,62 @@ int lttng_fix_pending_event_notifiers(void)
 	return 0;
 }
 
+static
+int copy_abi_counter_key(struct lttng_counter_key *key,
+		     const struct lttng_kernel_abi_counter_key *key_param)
+{
+	size_t i, j, nr_dimensions;
+
+	nr_dimensions = key_param->nr_dimensions;
+	if (nr_dimensions > LTTNG_KERNEL_ABI_COUNTER_DIMENSION_MAX)
+		return -EINVAL;
+	key->nr_dimensions = nr_dimensions;
+	for (i = 0; i < nr_dimensions; i++) {
+		const struct lttng_kernel_abi_counter_key_dimension *udim =
+			&key_param->key_dimensions[i];
+		struct lttng_counter_key_dimension *dim =
+			&key->key_dimensions[i];
+		size_t nr_key_tokens;
+
+		nr_key_tokens = udim->nr_key_tokens;
+		if (!nr_key_tokens || nr_key_tokens > LTTNG_KERNEL_ABI_NR_KEY_TOKEN)
+			return -EINVAL;
+		dim->nr_key_tokens = nr_key_tokens;
+		for (j = 0; j < nr_key_tokens; j++) {
+			const struct lttng_kernel_abi_key_token *utoken =
+				&udim->key_tokens[j];
+			struct lttng_key_token *token =
+				&dim->key_tokens[j];
+
+			switch (utoken->type) {
+			case LTTNG_KERNEL_ABI_KEY_TOKEN_STRING:
+			{
+				long ret;
+
+				token->type = LTTNG_KERNEL_ABI_KEY_TOKEN_STRING;
+				ret = strncpy_from_user(token->arg.string,
+					(char __user *)(unsigned long)utoken->arg.string_ptr,
+					LTTNG_KERNEL_ABI_KEY_TOKEN_STRING_LEN_MAX);
+				if (ret < 0)
+					return -EFAULT;
+				if (!ret || ret == LTTNG_KERNEL_ABI_KEY_TOKEN_STRING_LEN_MAX)
+					return -EINVAL;
+				break;
+			}
+			case LTTNG_KERNEL_ABI_KEY_TOKEN_EVENT_NAME:
+				token->type = LTTNG_KERNEL_ABI_KEY_TOKEN_EVENT_NAME;
+				break;
+			case LTTNG_KERNEL_ABI_KEY_TOKEN_PROVIDER_NAME:
+				printk(KERN_ERR "LTTng: Provider name token not supported.\n");
+				lttng_fallthrough;
+			default:
+				return -EINVAL;
+			}
+		}
+	}
+	return 0;
+}
+
 struct lttng_event_recorder_enabler *lttng_event_recorder_enabler_create(
 		enum lttng_enabler_format_type format_type,
 		struct lttng_kernel_abi_event *event_param,
@@ -2394,12 +2447,46 @@ struct lttng_event_recorder_enabler *lttng_event_recorder_enabler_create(
 	return event_enabler;
 }
 
+struct lttng_event_counter_enabler *lttng_event_counter_enabler_create(
+		enum lttng_enabler_format_type format_type,
+		struct lttng_kernel_abi_event *event_param,
+		const struct lttng_kernel_abi_counter_key *abi_key,
+		const struct lttng_counter_key *kernel_key,
+		struct lttng_kernel_channel_counter *chan)
+{
+	struct lttng_event_counter_enabler *event_enabler;
+
+	event_enabler = kzalloc(sizeof(*event_enabler), GFP_KERNEL);
+	if (!event_enabler)
+		return NULL;
+	event_enabler->parent.parent.enabler_type = LTTNG_EVENT_ENABLER_TYPE_COUNTER;
+	event_enabler->parent.parent.format_type = format_type;
+	INIT_LIST_HEAD(&event_enabler->parent.parent.filter_bytecode_head);
+	memcpy(&event_enabler->parent.parent.event_param, event_param,
+		sizeof(event_enabler->parent.parent.event_param));
+	event_enabler->chan = chan;
+	event_enabler->parent.chan = &chan->parent;
+	if (abi_key) {
+		if (copy_abi_counter_key(&event_enabler->key, abi_key)) {
+			kfree(event_enabler);
+			return NULL;
+		}
+	} else {
+		memcpy(&event_enabler->key, kernel_key, sizeof(*kernel_key));
+	}
+
+	/* ctx left NULL */
+	event_enabler->parent.parent.enabled = 0;
+	event_enabler->parent.parent.user_token = event_param->token;
+	return event_enabler;
+}
+
 void lttng_event_enabler_session_add(struct lttng_kernel_session *session,
-		struct lttng_event_recorder_enabler *event_enabler)
+		struct lttng_event_enabler_session_common *event_enabler)
 {
 	mutex_lock(&sessions_mutex);
-	list_add(&event_enabler->parent.parent.node, &session->priv->enablers_head);
-	event_enabler->parent.parent.published = true;
+	list_add(&event_enabler->parent.node, &session->priv->enablers_head);
+	event_enabler->parent.published = true;
 	lttng_session_lazy_sync_event_enablers(session);
 	mutex_unlock(&sessions_mutex);
 }